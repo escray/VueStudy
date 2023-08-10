26d5cb3f633576e5c9f0ba840e169308af973c68
escray <zescray@gmail.com>
2023-08-10 14:41:06 +0800

    course 06-1 click to add

---
+++ vite-admin/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3 @click="add">No. {{count}}</h3>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let count = ref(1)
+function add() {
+  count.value++
+}
+</script>
+
+<style>
+  h3 {
+    color:chocolate;
+  }
+</style>
--- vite-admin/src/pages/home.vue
+++ vite-admin/src/pages/home.vue
@@ -1,3 +1,8 @@
 <template>
   <h1>HOME</h1>
+  <TodoList />
 </template>
+
+<script setup>
+import TodoList from '../components/Todolist.vue';
+</script>
