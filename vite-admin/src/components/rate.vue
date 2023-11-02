04946bf2330fd57a9c401f010313b8ba3cec6f09
escray <zescray@gmail.com>
2023-11-02 19:42:47 +0800

    from home

--- vite-admin/src/components/rate.vue
+++ vite-admin/src/components/rate.vue
@@ -1,16 +1,28 @@
 <template>
   <div :style="fontstyle">
-    {{ rate }}
+    <div class='rate' @mouseout="mouseOut">
+      <span @mouseover="mouseOver(num)" v-for='num in 5' :key="num">☆</span>
+      <span class='hollow' :style="fontwidth">
+        <span @click="onRate(num)" @mouseovere="mouseOver(num)" v-for='num in 5' :key="num">★</span>
+      </span>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps, computed } from 'vue';
+import { defineProps, defineEmits, computed, ref } from 'vue';
 
 let props = defineProps({
   value: Number,
   theme: {type:String,default:'orange'}
 })
+
+let emits = defineEmits('update-rate')
+
+function onRate(num) {
+  emits('update-rate', num)
+}
+
 console.log(props)
 let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value))
 
@@ -25,4 +37,30 @@ const themeObj = {
 }
 
 const fontstyle = computed(()=> { return `color:${themeObj[props.theme]};`})
+
+let width = ref(props.value)
+function mouseOver(i) {
+  width.value = i
+}
+
+function mouseOut() {
+  width.value = props.value
+}
+
+const fontwidth = computed(()=>`width:${width.value}em;`)
+
 </script>
+<style scoped>
+.rate {
+  position: relative;
+  display: inline-block;
+}
+.rate > span.hollow {
+  position: absolute;
+  display: inline-block;
+  top: 0;
+  left: 0;
+  width: 0;
+  overflow: hidden;
+}
+</style>
--- vite-admin/src/pages/rate.vue
+++ vite-admin/src/pages/rate.vue
@@ -1,10 +1,18 @@
 <template>
-  <Rate :value="score"></Rate>
+  <h1>你的评分 {{ score }}</h1>
+  <Rate :value="score" @update-rate="update"></Rate>
+
   <Rate :value="4" theme="red"></Rate>
   <Rate :value="1" theme="green"></Rate>
+  <Rate :value="3.5"></Rate>
 </template>
 <script setup>
 import { ref } from 'vue'
 import Rate from '../components/rate.vue'
-let score = ref(3)
+
+let score = ref(3.5)
+
+function update(num) {
+  score.value = num
+}
 </script>
