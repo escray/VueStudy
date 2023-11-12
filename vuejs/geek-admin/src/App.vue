fe42d9530f37369b0e3ec59a6f25af1c111661ca
escray <zescray@gmail.com>
2023-11-12 16:36:06 +0800

    finished lesson 19

--- vuejs/geek-admin/src/App.vue
+++ vuejs/geek-admin/src/App.vue
@@ -1,6 +1,38 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import eCharts from './components/eCharts.vue'
+import { ref } from 'vue'
+
+// function fib(n) {
+//   if (n <= 1) {
+//     return 1
+//   }
+//   return fib(n-1) + fib(n-2)
+// }
+
+function fib(n) {
+  let arr = [1, 1]
+  let i = 2
+  while (i <= n) {
+    arr[i] = arr[i - 1] + arr[i - 2]
+    i++
+  }
+  return arr[n]
+}
+
+let timing = window.performance && window.performance.timing
+let navigation = window.performance && window.performance.navigation
+// DNS 解析：
+let dns = timing.domainlookupEnd - timing.domainLookupStart
+// 总体网络交互耗时：
+let network = timing.responseEnd - timing.navigationStart
+// 渲染处理：
+let processing = (timing.domComplete || timing.domLoading) - timing.domLoading
+// 可交互：
+let active = timing.domInteractive - timing.navigationStart
+
+let count = ref(fib(38))
+
 </script>
 
 <template>
