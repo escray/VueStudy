b3e3f35b690d31828f5e365120c7b97d1d8750f5
escray <zescray@gmail.com>
2023-11-16 22:18:45 +0800

    mixin.scss miss $ so that error layout

--- vite-ts/src/App.vue
+++ vite-ts/src/App.vue
@@ -29,6 +29,16 @@
     </el-container>
   </el-container>
   <hr />
+  <el-container>
+    <el-aside width="200px">
+      Aside
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
 
 </template>
 
