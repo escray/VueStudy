82170ae269358b7c43be45173520e4a6b182b5c7
escray <zescray@gmail.com>
2023-11-18 11:32:49 +0800

    all testes passed finally

--- vite-ts/jest.config.js
+++ vite-ts/jest.config.js
@@ -3,9 +3,13 @@ module.exports = {
     //  用 `vue-jest` 处理 `*.vue` 文
     '^.+\\.vue$': '@vue/vue3-jest', //vue-jest 处理.vue
     '^.+\\.jsx?$': 'babel-jest', // babel jest处理js or jsx
-    // '^.+\\.tsx?$': 'ts-jest', // ts-jest 处理.ts.tsx
+    '^.+\\.tsx?$': 'ts-jest', // ts-jest 处理.ts.tsx
   },
   testMatch: ['**/?(*.)+(spec).[jt]s?(x)'],
-  "collectCoverage": true,
-  "coverageReporters": ["json", "html"]
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"]
+  }
 }
