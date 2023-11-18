861c9bc970d8ace60c53c4660a18acbdb5675390
escray <zescray@gmail.com>
2023-11-18 10:42:52 +0800

    resolve TS2307 and jest config

--- vite-ts/jest.config.js
+++ vite-ts/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   transform: {
     //  用 `vue-jest` 处理 `*.vue` 文
-    '^.+\\.vue$': 'vue-jest', //vue-jest 处理.vue
+    '^.+\\.vue$': '@vue/vue3-jest', //vue-jest 处理.vue
     '^.+\\.jsx?$': 'babel-jest', // babel jest处理js or jsx
-    '^.+\\.tsx?$': 'ts-jest', // ts-jest 处理.ts.tsx
+    // '^.+\\.tsx?$': 'ts-jest', // ts-jest 处理.ts.tsx
   },
   testMatch: ['**/?(*.)+(spec).[jt]s?(x)'],
   "collectCoverage": true,
