a65e665c32bd47256d35435ac76c989c7342d598
escray <zescray@gmail.com>
2023-12-19 13:41:01 +0800

    dialog

---
+++ vue3-practice/advance/components-theme/packages/components/src/dialog/dialog.tsx
@@ -0,0 +1,24 @@
+import { defineComponent } from 'vue'
+import { prefixName } from '../theme/index'
+
+export const DialogComponent = defineComponent({
+  props: {
+    text: String
+  },
+  emits: ['onOk'],
+  setup(props, context) {
+    const { emit } = context
+    const onOk = () => {
+      emit('onOk')
+    }
+    return () => {
+      return (
+        <div class={`${prefixName}-dialog-mask`}>
+          <div class={`${prefixName}-dialog`}>
+            <div class={`${prefixName}-dialog-text`}>{props.text}</div>
+          </div>
+        </div>
+      )
+    }
+  }
+})
