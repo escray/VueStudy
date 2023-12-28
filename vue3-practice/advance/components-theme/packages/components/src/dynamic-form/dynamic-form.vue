b8b06c2c60358ef9789afe482d91915bc7d7653b
escray <zescray@gmail.com>
2023-12-28 08:02:38 +0800

    from home dynamic-form

---
+++ vue3-practice/advance/components-theme/packages/components/src/dynamic-form/dynamic-form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="{ [baseClassName]: true }">
+    <Form>
+      <Form
+        ref="formRef"
+        :model="internalModel"
+        @finish="onFinish"
+        @finishFail="onfinishFail"
+      >
+        <FormItem>
+
+        </FormItem>
+      </Form>
+    </Form>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { prefixName } from '../theme'
+import Row from '../row'
+import Form from '../form'
+import Input from '../input'
+
+</script>
