c2853fa7d879bf2753811a10de9032a8968ae1cb
escray <zescray@gmail.com>
2024-01-10 18:14:20 +0800

    finish lesson 16

--- vue3-practice/advance/components-theme/packages/work-front/src/index.ts
+++ vue3-practice/advance/components-theme/packages/work-front/src/index.ts
@@ -14,36 +14,67 @@ import AboutView from './views/about.vue'
 import DetailView from './views/detail.vue'
 import DetailItemView from './views/detail-item.vue'
 
-// define route
-// 定义路由
+// // define route
+// // 定义路由
+// const router = createRouter({
+//   linkActiveClass: 'active',
+//   // hash 路由配置
+//   // history: createWebHashHistory('/'),
+//   // history 路由配置
+//   history: createWebHistory('/'),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView
+//     },
+//     {
+//       path: '/list',
+//       name: 'list',
+//       component: ListView
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       component: AboutView
+//     },
+//     // 动态路由
+//     {
+//       path: '/detail/:id',
+//       component: DetailView,
+//       // 嵌套路由
+//       children: [{ path: '', component: DetailItemView }]
+//     }
+//   ]
+// })
+
+// 利用 ES Module 的动态 import 模块的能力，通过 import 来动态加载模块
 const router = createRouter({
   linkActiveClass: 'active',
-  // hash 路由配置
-  // history: createWebHashHistory('/'),
   // history 路由配置
   history: createWebHistory('/'),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('./views/home.vue')
     },
     {
       path: '/list',
       name: 'list',
-      component: ListView
+      component: () => import('./views/list.vue')
     },
     {
       path: '/about',
       name: 'about',
-      component: AboutView
+      component: () => import('./views/about.vue')
     },
-    // 动态路由
     {
       path: '/detail/:id',
-      component: DetailView,
-      // 嵌套路由
-      children: [{ path: '', component: DetailItemView }]
+      component: () => import('./views/detail.vue'),
+      children: [
+        { path: '', component: () => import('./views/detail-item.vue')}
+      ]
     }
   ]
 })
--- vue3-practice/advance/components-theme/packages/work-front/src/views/detail-item.vue
+++ vue3-practice/advance/components-theme/packages/work-front/src/views/detail-item.vue
@@ -6,6 +6,7 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 
+// useRouter 让开发者能主动操作路由实体，例如可以触发路由跳转等
 const router = useRouter()
 
 const onClick = () => {
